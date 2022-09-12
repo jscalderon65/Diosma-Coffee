3cde047f6137a90c1ea5ebe498e481e95fd3553e
jscalderon65 <jhonnseca0599@gmail.com>
2022-09-12 10:06:10 -0500

    Ajustes de estilos

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,108 +1,78 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title style="text-align: center;" class="text-h5">
+            <b>Diosma Café</b>
+          </v-list-item-title>
+          <!--           <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle> -->
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item  v-for="item in items" :key="item.title" :to="item.to"  @click="drawer = false">
+          <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
+          </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <div style="width: 100%;display:flex;justify-content: flex-end ;">
-        <div style="border:solid;height:100px;width: 100px;background-color: aliceblue;display: flex;justify-content: center;align-items: flex-end;border-radius: 5px;">
-          <v-icon style="font-size: 4rem;color: black;margin-bottom: 5px;">mdi-coffee</v-icon></div>
+        <div
+          style="border:solid;height:100px;width: 100px;background-color: aliceblue;display: flex;justify-content: center;align-items: flex-end;border-radius: 10px;">
+          <v-icon style="font-size: 4rem;color: black;margin-bottom: 5px;">mdi-coffee</v-icon>
         </div>
+      </div>
     </v-app-bar>
     <v-main>
-      <Nuxt/>
+      <Nuxt />
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
+    <v-footer dark padless>
+      <v-card dark style="width: 100vw;" class="white--text text-center">
+        <v-card-text>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+            <v-icon size="24px">
+              {{ icon }}
             </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-    dark
-    padless
-  >
-    <v-card
-    dark
-    style="width: 100vw;"
-      class="white--text text-center"
-    >
-      <v-card-text>
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4 white--text"
-          icon
-        >
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
-      </v-card-text>
-      <v-divider></v-divider>
+          </v-btn>
+        </v-card-text>
+        <v-divider></v-divider>
 
-      <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-card-text>
-    </v-card>
-  </v-footer>
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
   name: 'DefaultLayout',
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-silverware',
+          title: 'Menú',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
+          icon: 'mdi-coffee',
+          title: 'Nosotros',
           to: '/inspire'
         }
       ],
@@ -120,3 +90,6 @@ export default {
   }
 }
 </script>
+<style scoped>
+
+</style>
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,21 +1,21 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
+  <div style="height: 100vh;display: flex;justify-content: center;align-items: center;">
+    <div style="text-align:center">
       <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
+          src="/v.png"
+          alt="Vuetify.js"
+          class="mb-5"
+        >
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+    </div>
+</div>
 </template>
 
 <script>
